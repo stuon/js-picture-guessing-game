6f5fdbae40a90d490376806eeb967153aa76bc03
San Tuon <san.tuon@gmail.com>
2020-07-25 11:15:39 +1000

    code cleanup

--- js/app.js
+++ js/app.js
@@ -15,71 +15,36 @@ function repeatXI(callback, interval, repeats, immediate) {
   }
 }
 
-const ROWS = 10;
-const COLS = 10;
+var app = (function () {
+  const ROWS = 10;
+  const COLS = 10;
+  const BOARD_MAX_WIDTH = 600;
+  const BOARD_MAX_HEIGHT = 800;
 
-const BOARD_MAX_WIDTH = 600;
-const BOARD_MAX_HEIGHT = 800;
+  var canvas = null;
+  var img = null;
+  var ctx = null;
+  var columnSize = 0;
+  var rowSize = 0;
 
-var board = new Board();
-board.init(ROWS, COLS);
-
-function loadBoard() {
-  var canvas = this.document.createElement("canvas");
-  canvas.width = BOARD_MAX_WIDTH;
-  canvas.height = BOARD_MAX_HEIGHT;
-
-  board.reset();
-
-  var game = document.querySelector("#game");
-  game.insertBefore(canvas, game.childNodes[0]);
-
-  var ctx = canvas.getContext("2d");
-
-  var img = new Image();
-  img.onload = start;
-  img.src =
-    "https://www.joblo.com/assets/images/joblo/posters/2020/03/coffeekareempost1.jpg";
-
-  function start() {
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    const columnSize = Math.ceil(canvas.width / COLS);
-    const rowSize = Math.ceil(canvas.height / ROWS);
-
-    ctx.beginPath();
-    ctx.strokeStyle = "rgba(0,0,0,0.7)";
-    ctx.rect(0, 0, canvas.width, canvas.height);
-    ctx.stroke();
-
-    document.getElementById("clear").onclick = function () {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.beginPath();
-      ctx.strokeStyle = "rgba(0,0,0,0.7)";
-      ctx.rect(0, 0, canvas.width, canvas.height);
-      ctx.stroke();
-
-      board.reset();
+  var getMousePos = function (canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top,
     };
+  };
 
-    function getMousePos(canvas, evt) {
-      var rect = canvas.getBoundingClientRect();
-      return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top,
-      };
-    }
+  var writeMessage = function (canvas, message) {
+    //console.log(message);
+    /*  var context = canvas.getContext("2d");
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.font = "18pt Calibri";
+        context.fillStyle = "black";
+        context.fillText(message, 10, 25);*/
+  };
 
-    function writeMessage(canvas, message) {
-      //console.log(message);
-      /*  var context = canvas.getContext("2d");
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.font = "18pt Calibri";
-      context.fillStyle = "black";
-      context.fillText(message, 10, 25);*/
-    }
-
+  var setupEventListeners = function () {
     canvas.addEventListener(
       "mousemove",
       function (evt) {
@@ -90,74 +55,15 @@ function loadBoard() {
       false
     );
 
-    function clickShowCell() {
-      var position = board.getRandomAvailableCell();
-      showShow(position);
-    }
+    document.getElementById("clear").onclick = function () {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.strokeStyle = "rgba(0,0,0,0.7)";
+      ctx.rect(0, 0, canvas.width, canvas.height);
+      ctx.stroke();
 
-    function showShow(position) {
-      if (position === null) return;
-
-      const posX = position.x * columnSize;
-      const posY = position.y * rowSize;
-
-      // if last row/column then use the remainder width
-      const widthAdj =
-        position.x + 1 === COLS ? canvas.width - posX : columnSize;
-
-      const heightAdj =
-        position.y + 1 === ROWS ? canvas.height - posY : rowSize;
-
-      board.showCell(position.x, position.y, true);
-
-      ctx.drawImage(
-        img,
-        posX,
-        posY,
-        widthAdj,
-        heightAdj,
-        posX,
-        posY,
-        widthAdj,
-        heightAdj
-      );
-
-      if (!position.topSet) {
-        ctx.beginPath();
-        ctx.lineWidth = 0.5;
-        ctx.moveTo(posX, posY);
-        ctx.lineTo(posX + widthAdj, posY);
-        ctx.strokeStyle = "rgba(0,0,0,0.7)";
-        ctx.stroke();
-      }
-
-      if (!position.leftSet) {
-        ctx.beginPath();
-        ctx.lineWidth = 0.5;
-        ctx.moveTo(posX, posY);
-        ctx.lineTo(posX, posY + heightAdj);
-        ctx.strokeStyle = "rgba(0,0,0,0.7)";
-        ctx.stroke();
-      }
-
-      if (!position.rightSet) {
-        ctx.beginPath();
-        ctx.lineWidth = 0.5;
-        ctx.moveTo(posX + widthAdj, posY);
-        ctx.lineTo(posX + widthAdj, posY + heightAdj);
-        ctx.strokeStyle = "rgba(0,0,0,0.7)";
-        ctx.stroke();
-      }
-
-      if (!position.bottomSet) {
-        ctx.beginPath();
-        ctx.lineWidth = 0.5;
-        ctx.moveTo(posX, posY + heightAdj);
-        ctx.lineTo(posX + widthAdj, posY + heightAdj);
-        ctx.strokeStyle = "rgba(0,0,0,0.7)";
-        ctx.stroke();
-      }
-    }
+      board.reset();
+    };
 
     document.getElementById("autorun").onclick = function () {
       const remainingCount = board.remainingCells();
@@ -182,5 +88,109 @@ function loadBoard() {
       ctx.rect(0, 0, canvas.width, canvas.height);
       ctx.stroke();
     };
-  }
-}
+  };
+
+  var start = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    columnSize = Math.ceil(canvas.width / COLS);
+    rowSize = Math.ceil(canvas.height / ROWS);
+
+    ctx.beginPath();
+    ctx.strokeStyle = "rgba(0,0,0,0.7)";
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.stroke();
+  };
+
+  var clickShowCell = function () {
+    var position = board.getRandomAvailableCell();
+    showCell(position);
+  };
+
+  var showCell = function (position) {
+    if (position === null) return;
+
+    const posX = position.x * columnSize;
+    const posY = position.y * rowSize;
+
+    // if last row/column then use the remainder width
+    const widthAdj = position.x + 1 === COLS ? canvas.width - posX : columnSize;
+
+    const heightAdj = position.y + 1 === ROWS ? canvas.height - posY : rowSize;
+
+    board.showCell(position.x, position.y, true);
+
+    ctx.drawImage(
+      img,
+      posX,
+      posY,
+      widthAdj,
+      heightAdj,
+      posX,
+      posY,
+      widthAdj,
+      heightAdj
+    );
+
+    if (!position.topSet) {
+      ctx.beginPath();
+      ctx.lineWidth = 0.5;
+      ctx.moveTo(posX, posY);
+      ctx.lineTo(posX + widthAdj, posY);
+      ctx.strokeStyle = "rgba(0,0,0,0.7)";
+      ctx.stroke();
+    }
+
+    if (!position.leftSet) {
+      ctx.beginPath();
+      ctx.lineWidth = 0.5;
+      ctx.moveTo(posX, posY);
+      ctx.lineTo(posX, posY + heightAdj);
+      ctx.strokeStyle = "rgba(0,0,0,0.7)";
+      ctx.stroke();
+    }
+
+    if (!position.rightSet) {
+      ctx.beginPath();
+      ctx.lineWidth = 0.5;
+      ctx.moveTo(posX + widthAdj, posY);
+      ctx.lineTo(posX + widthAdj, posY + heightAdj);
+      ctx.strokeStyle = "rgba(0,0,0,0.7)";
+      ctx.stroke();
+    }
+
+    if (!position.bottomSet) {
+      ctx.beginPath();
+      ctx.lineWidth = 0.5;
+      ctx.moveTo(posX, posY + heightAdj);
+      ctx.lineTo(posX + widthAdj, posY + heightAdj);
+      ctx.strokeStyle = "rgba(0,0,0,0.7)";
+      ctx.stroke();
+    }
+  };
+
+  return {
+    init: function () {
+      board.init(ROWS, COLS);
+      board.reset();
+
+      canvas = document.createElement("canvas");
+      canvas.width = BOARD_MAX_WIDTH;
+      canvas.height = BOARD_MAX_HEIGHT;
+
+      var game = document.querySelector("#game");
+      game.insertBefore(canvas, game.childNodes[0]);
+
+      ctx = canvas.getContext("2d");
+      setupEventListeners();
+
+      img = new Image();
+      img.onload = start;
+      img.src =
+        "https://www.joblo.com/assets/images/joblo/posters/2020/03/coffeekareempost1.jpg";
+    },
+  };
+})();
+
+app.init();
--- js/board.js
+++ js/board.js
@@ -2,76 +2,71 @@
 Board for pixel color game
 */
 
-var Board = function Board() {
+var board = (function Board() {
   this.columns = 0; // x position
   this.rows = 0; // y position
   this.imageSizeMultiplier = 0;
   this.cellsShownCount = 0;
   this.cellsShown = null;
-};
 
-Board.prototype.init = function (columns, rows) {
-  this.columns = columns; // x positions
-  this.rows = rows; // y positions
-};
-
-Board.prototype.remainingCells = function () {
-  return this.rows * this.columns - this.cellsShownCount;
-};
-
-Board.prototype.showCell = function (x, y, show) {
-  this.cellsShown[y * this.rows + x] = show;
-  this.cellsShownCount += show ? 1 : -1;
-};
-
-Board.prototype.reset = function () {
-  this.cellsShownCount = 0;
-  this.cellsShown = new Array(this.rows * this.columns);
-};
-
-Board.prototype.isShowCell = function (x, y) {
-  if (x < 0 || y < 0 || x >= this.columns || y >= this.rows) return false;
-
-  return this.cellsShown[y * this.rows + x] || false;
-};
-
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max + 1 - min)) + min;
-}
-
-Board.prototype.getCell = function (x, y) {
   return {
-    x: x,
-    y: y,
-    topSet: this.isShowCell(x, y - 1),
-    leftSet: this.isShowCell(x - 1, y),
-    rightSet: this.isShowCell(x + 1, y),
-    bottomSet: this.isShowCell(x, y + 1),
+    init: function (columns, rows) {
+      this.columns = columns; // x positions
+      this.rows = rows; // y positions
+    },
+    remainingCells: function () {
+      return this.rows * this.columns - this.cellsShownCount;
+    },
+    showCell: function (x, y, show) {
+      this.cellsShown[y * this.rows + x] = show;
+      this.cellsShownCount += show ? 1 : -1;
+    },
+    reset: function () {
+      this.cellsShownCount = 0;
+      this.cellsShown = new Array(this.rows * this.columns);
+    },
+    isShowCell: function (x, y) {
+      if (x < 0 || y < 0 || x >= this.columns || y >= this.rows) return false;
+
+      return this.cellsShown[y * this.rows + x] || false;
+    },
+    getRndInteger: function (min, max) {
+      return Math.floor(Math.random() * (max + 1 - min)) + min;
+    },
+    getCell: function (x, y) {
+      return {
+        x: x,
+        y: y,
+        topSet: this.isShowCell(x, y - 1),
+        leftSet: this.isShowCell(x - 1, y),
+        rightSet: this.isShowCell(x + 1, y),
+        bottomSet: this.isShowCell(x, y + 1),
+      };
+    },
+    getRandomAvailableCell: function () {
+      const randomIndex = this.getRndInteger(
+        0,
+        this.rows * this.columns - 1 - this.cellsShownCount
+      );
+
+      let num = 0;
+      let x = 0;
+      let y = 0;
+
+      for (y = 0; y < this.rows; y++) {
+        for (x = 0; x < this.columns; x++) {
+          if (this.isShowCell(x, y)) {
+            continue;
+          }
+
+          if (num == randomIndex) {
+            return this.getCell(x, y);
+          }
+          num++;
+        }
+      }
+
+      return null;
+    },
   };
-};
-
-Board.prototype.getRandomAvailableCell = function () {
-  const randomIndex = getRndInteger(
-    0,
-    this.rows * this.columns - 1 - this.cellsShownCount
-  );
-
-  let num = 0;
-  let x = 0;
-  let y = 0;
-
-  for (y = 0; y < this.rows; y++) {
-    for (x = 0; x < this.columns; x++) {
-      if (this.isShowCell(x, y)) {
-        continue;
-      }
-
-      if (num == randomIndex) {
-        return this.getCell(x, y);
-      }
-      num++;
-    }
-  }
-
-  return null;
-};
+})();
